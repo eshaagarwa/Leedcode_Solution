00b13ecd432b31bc1325b723c218eb1a727c8bd3
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-06-21 05:26:34 +0530

    Time: 676 ms (97.93%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH
+  AccumulatedQueue AS (
+    SELECT
+      person_name,
+      SUM(weight) OVER(ORDER BY turn) AS accumulated_weight
+    FROM Queue
+  )
+SELECT person_name
+FROM AccumulatedQueue
+WHERE accumulated_weight <= 1000
+ORDER BY accumulated_weight DESC
+LIMIT 1;
