e2e11a646d628488a8d41bfdd72cd03da24d2de9
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-06-21 05:10:24 +0530

    Time: 354 ms (95.82%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT Today.id
+FROM Weather AS Today
+INNER JOIN Weather AS Yesterday
+  ON (DATE_SUB(Today.recordDate, INTERVAL 1 DAY) = Yesterday.recordDate)
+WHERE Today.temperature > Yesterday.temperature;
