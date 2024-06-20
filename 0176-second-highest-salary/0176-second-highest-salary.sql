24b9cb632a710eb0b6bce6b165edaad1f0e38eb1
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-06-21 05:01:42 +0530

    Time: 231 ms (92.30%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH
+  RankedEmployees AS (
+    SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS `rank`
+    FROM Employee
+  )
+SELECT MAX(salary) AS SecondHighestSalary
+FROM RankedEmployees
+WHERE `rank` = 2;
