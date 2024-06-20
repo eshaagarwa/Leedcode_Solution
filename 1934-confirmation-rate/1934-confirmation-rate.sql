533dce0f7ddf5a3798e91a203e43e776ce793fd6
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-06-21 05:14:37 +0530

    Time: 554 ms (96.20%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+  Signups.user_id,
+  IFNULL(ROUND(AVG(Confirmations.action = 'confirmed'), 2), 0) AS confirmation_rate
+FROM Signups
+LEFT JOIN Confirmations
+  USING (user_id)
+GROUP BY 1;
