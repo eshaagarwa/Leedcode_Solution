3cfe78c905285247f7599f0a326f72a43c924a66
ESHA AGARWAL <80843306+eshaagarwa@users.noreply.github.com>
2024-07-15 15:10:50 +0530

    Time: 66 ms (65.23%), Space: 55.6 MB (63.24%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public TreeNode createBinaryTree(int[][] descriptions) {
+    Map<TreeNode, TreeNode> childToParent = new HashMap<>();
+    Map<Integer, TreeNode> valToNode = new HashMap<>();
+
+    for (int[] d : descriptions) {
+      final int p = d[0];
+      final int c = d[1];
+      final int isLeft = d[2];
+      TreeNode parent = valToNode.getOrDefault(p, new TreeNode(p));
+      TreeNode child = valToNode.getOrDefault(c, new TreeNode(c));
+      valToNode.put(p, parent);
+      valToNode.put(c, child);
+      childToParent.put(child, parent);
+      if (isLeft == 1)
+        parent.left = child;
+      else
+        parent.right = child;
+    }
+
+    // Pick a random node and traverse upwardly.
+    TreeNode root = childToParent.keySet().iterator().next();
+    while (childToParent.containsKey(root))
+      root = childToParent.get(root);
+    return root;
+  }
+}
